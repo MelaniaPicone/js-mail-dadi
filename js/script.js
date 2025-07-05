5726405517626643d0673ec82203ca4fa900dfc6
MelaniaPicone <melania_picone@yahoo.it>
2025-07-05 11:05:41 +0200

    [add] mail list and for

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// array con email
+const mailGuest = [
+  'anna.g@yahoo.it', 
+  'antonio.rossi@gmail.com', 
+  'carlo.barbero@aruba.it', 
+  'ele.landi@yahoo.com', 
+  'fiore.milano@gmail.it', 
+  'francesco.solerte@aruba.com'];
+
+console.log(mailGuest);
+
+  // inserisci la tua mail
+  const userMail = parseInt(prompt('Inserisci qui il tuo contatto'));
+  let = notFind;
+
+// ciclo for delle email
+
+
+
+
+
+
+
+}
